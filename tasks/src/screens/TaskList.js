14de2a8b156da99c208024c5686278d04ce72424
dfbrasil <danieldfb@gmail.com>
2024-01-29 19:46:40 -0300

    segundo commit

--- tasks/src/screens/TaskList.js
+++ tasks/src/screens/TaskList.js
@@ -1,12 +1,21 @@
 import React, { Component } from "react";
-import { SafeAreaView, Text, StyleSheet } from "react-native";
+import { SafeAreaView, Text, StyleSheet, ImageBackground, View } from "react-native";
 import { StatusBar } from 'expo-status-bar';
+
+import todayImage from '../../assets/imgs/today.jpg'
 
 export default class TaskList extends Component {
     render(){
         return(
             <SafeAreaView style={styles.container}>
-                <Text> Dentro de TaskList </Text>
+                <ImageBackground 
+                    source={todayImage}
+                    style={styles.background}>
+                </ImageBackground>
+                <View style={styles.taskList}>
+                    <Text> Dentro de TaskList </Text>
+                </View>
+                
                 <StatusBar style="auto" />
             </SafeAreaView>
         )
@@ -15,9 +24,12 @@ export default class TaskList extends Component {
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
+        flex: 1
     },
+    background: {
+        flex: 3
+    },
+    taskList: {
+        flex:7
+    }
   });
